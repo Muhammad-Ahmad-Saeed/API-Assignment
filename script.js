65d2adce59f88d6c82bc62a78e74ea2bf365a240
Muhammad Ahmad Saeed <ahmadsaeed3220@gmail.com>
2023-11-08 11:23:44 +0500

    Add load recies function

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function () {
+    loadRecipies();
+});
+
+function loadRecipies() {
+    $.ajax({
+      url: "https://usman-fake-api.herokuapp.com/api/recipes",
+      method: "GET",
+      error: function (response) {
+        var recipes = $("#recipes");
+        recipes.html("An Error has occured");
+      },
+      success: function (response) {
+        console.log(response);
+        var recipes = $("#recipes");
+        recipes.empty();
+        for (var i = 0; i < response.length; i++) {
+          var rec = response[i];
+          recipes.append(
+            `<div class="recipe" data-id="${rec._id}"><h3>${rec.title}</h3><p><button class="btn btn-danger btn-sm float-right">delete</button><button class="btn btn-warning btn-sm float-right">Edit</button> ${rec.body}</p></div>`
+          );
+        }
+      },
+    });
+  }
